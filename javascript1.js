ef30d4d909f6c34c8b6fcfd60af0a9aa019ec48d
O-schutte15 <jonathan.schutte@msj.edu>
2026-09-12 16:24:08 -0400

    Added Requirments for JavaScript 1

---
+++ javascript1.js
@@ -0,0 +1,8 @@
+console.log("This is javascript page 1");
+
+// Jonathan Schutte's Favorite color is Hunter Green\
+
+let cookies = 5;
+const people = 2;
+cookies = cookies - people;
+console.log(`There are currently ${cookies} cookies left.`);
